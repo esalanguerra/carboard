efa4672ab5cfe87ac5960ee839deb12e1334da50
Alan Guerra <esalanguerra@gmail.com>
2024-12-14 13:04:12 -0300

    retornando dados de carros

--- api/main.py
+++ api/main.py
@@ -5,3 +5,116 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "ok"}
+
+@app.get("/carros")
+def get_cars():
+    cars = [
+        {
+            "nome": "Ford Focus",
+            "ID": "ID 14754696",
+            "placa": "FIY-463 - Vaasa, Pohjanmaa",
+            "vendedor": "Joel Algren",
+            "preco": "1 350 €",
+            "data": "Päivitetty 13.12.2024",
+            "condicao": "Não encontrado",
+            "contato": "+358465881143",
+            "quilometragem": "423 000 km",
+            "ano": "2008 (ensirek. 9-2008)",
+            "motor": "1.6 l, Diesel",
+            "cambio": "Manuaali",
+            "proprietarios": "Não encontrado",
+            "inspecionado": "12-2024",
+            "sistema_de_transmissao": "Etuveto",
+            "especificacoes": {
+                "teho": "66 kW / 90 Hv",
+                "torque": "215 Nm",
+                "velocidade_maxima": "177 km/h",
+                "aceleracao_0_100": "12,90 s",
+                "peso": "1 386 kg",
+                "consumo": {
+                    "cidade": "5,6 l/100 km",
+                    "estrada": "3,8 l/100 km",
+                    "combinado": "4,5 l/100 km"
+                }
+            },
+            "seguranca": [
+                "Ajonvakautusjärjestelmä",
+                "Luistonestojärjestelmä",
+                "Sumuvalot",
+                "Varashälytin",
+                "Käynnistyksenesto",
+                "Lukkiutumattomat jarrut (ABS)",
+                "Turvatyynyt"
+            ],
+            "interior_comodidades": [
+                "Ajotietokone",
+                "Kaadettavat takaistuimet",
+                "Ohjaustehostin",
+                "Sähkökäyttöiset ikkunat",
+                "Ilmastointi: Manuaalinen",
+                "Penkinlämmittimet"
+            ],
+            "eletronica": [
+                "Äänentoistojärjestelmä"
+            ],
+            "informacoes_adicionais": "Myyntiin juuri leimattu Ford Focus. Auto on suht siistissä kunnossa sisältä ja päältä. 423tkm, ei kilisee tai kolise.",
+            "outros": [
+                "Kahdet renkaat",
+                "Sadetunnistin",
+                "Vetokoukku",
+                "Turbo"
+            ],
+            "imagem": "https://images.nettiauto.com/live/2024/12/13/54b7bc5aa257bb69-medium.jpg"
+        },
+        {
+            "nome": "BMW M550d",
+            "ID": "ID 14754695",
+            "placa": "XNL-613 - Kittilä, Lappi",
+            "vendedor": "Arttu Majava",
+            "preco": "22 500 €",
+            "data": "Päivitetty 13.12.2024",
+            "condicao": "Sport A xDrive F11 Touring",
+            "contato": "0409643694",
+            "quilometragem": "240 000 km",
+            "ano": "2012 (ensirek. 12-2012)",
+            "motor": "3.0 l, Diesel",
+            "cambio": "Automaatti",
+            "proprietarios": "Não encontrado",
+            "inspecionado": "Não encontrado",
+            "sistema_de_transmissao": "Neliveto",
+            "especificacoes": {
+                "teho": "280 kW / 381 Hv",
+                "torque": "740 Nm",
+                "velocidade_maxima": "250 km/h",
+                "aceleracao_0_100": "4,7 s",
+                "peso": "2 065 kg",
+                "consumo": {
+                    "cidade": "7,2 l/100 km",
+                    "estrada": "6,0 l/100 km",
+                    "combinado": "6,4 l/100 km"
+                }
+            },
+            "seguranca": [
+                "Ajonvakautusjärjestelmä",
+                "Lukkiutumattomat jarrut (ABS)",
+                "Turvatyynyt"
+            ],
+            "interior_comodidades": [
+                "Vakionopeudensäädin",
+                "Sähköpeilit",
+                "Ilmastointi: Automaattinen"
+            ],
+            "eletronica": [
+                "Navigointijärjestelmä",
+                "Äänentoistojärjestelmä"
+            ],
+            "informacoes_adicionais": "Erittäin siisti auto, jossa hyvä huoltohistoria.",
+            "outros": [
+                "Sadetunnistin",
+                "Panoraamakatto",
+                "Vetokoukku"
+            ],
+            "imagem": "https://images.nettiauto.com/live/2024/12/13/54b7bc5aa257bb70-medium.jpg"
+        }
+    ]
+    return cars
