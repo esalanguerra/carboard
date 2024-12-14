15aab5b13eb65d9c5011aeaef925d4a2e7945dc5
Alan Guerra <esalanguerra@gmail.com>
2024-12-14 16:06:04 -0300

    Adicionando configs

--- server/src/lib/server/index.ts
+++ server/src/lib/server/index.ts
@@ -6,22 +6,31 @@ import { UsersRouter } from "../infra/routes/user-routes";
 
 export class Server {
   public readonly app: express.Application;
-
   public readonly usersRouter: UsersRouter;
 
   constructor() {
     this.app = express();
-
     this.usersRouter = new UsersRouter();
   }
 
   public async start() {
     this.app.use(compression());
-
-    this.app.use(helmet());
+    this.app.use(
+      helmet({
+        contentSecurityPolicy: false,
+        crossOriginEmbedderPolicy: false,
+      })
+    );
+    this.app.use(helmet.xssFilter());
+    this.app.use(helmet.noSniff());
+    this.app.use(helmet.frameguard({ action: "deny" }));
+    this.app.use(helmet.referrerPolicy({ policy: "strict-origin" }));
 
     this.app.use(
       cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         exposedHeaders: ["x-total-count", "Content-Type", "Content-Length"],
       })
     );
@@ -33,7 +42,7 @@ export class Server {
 
     this.app.listen(process.env.APP_PORT || 3333, async () => {
       console.log(
-        `Main server has ben started on port *${process.env.APP_PORT}`
+        `Main server has been started on port *${process.env.APP_PORT}`
       );
     });
   }
