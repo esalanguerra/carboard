6efcc2c32a409e358a3f9d853c582700fe3d17f6
Alan Guerra <esalanguerra@gmail.com>
2024-12-18 14:42:28 -0300

    corrijindo backend

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -4,12 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TemplateMessagesModule } from './template-messages/template-messages.module';
 import { MessagesModule } from './messages/messages.module';
-import { CustomersModule } from '../../../server/src/customers/customers.module';
 import { ConfigModule } from '@nestjs/config';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, TemplateMessagesModule, MessagesModule, CustomersModule, CarsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, TemplateMessagesModule, MessagesModule, CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/backend/src/cars/cars.service.ts
+++ src/backend/src/cars/cars.service.ts
@@ -6,14 +6,22 @@ export class CarsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async findAll() {
-    return await this.prismaService.car.findMany();
+    const cars = await this.prismaService.car.findMany();
+
+    return cars;
   }
 
   async findOne(id: string) {
-    return await this.prismaService.car.findUnique({
+    const car = await this.prismaService.car.findUnique({
       where: {
         id_car: id,
       },
     });
+
+    if (!car) {
+      throw new Error("Car not found");
+    }
+
+    return car;
   }
 }
