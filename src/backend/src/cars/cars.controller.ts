43d66b4324e503eff3986049e401e6bdddc7b036
Alan Guerra <esalanguerra@gmail.com>
2024-12-19 00:16:57 -0300

    adicionando filtros

--- src/backend/src/cars/cars.controller.ts
+++ src/backend/src/cars/cars.controller.ts
@@ -1,20 +1,52 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
-import { CarsService } from './cars.service';
+import { Controller, Get, Param, Query } from "@nestjs/common";
+import { CarsService } from "./cars.service";
 
-@Controller('cars')
+@Controller("cars")
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.carsService.findOne(id);
-  }
-
   @Get()
-  findAllPagination(
-    @Query('page') page: number,
-    @Query('perPage') perPage: number,
+  findAll(
+    @Query("page") page?: number,
+    @Query("perPage") perPage?: number,
   ) {
     return this.carsService.findAll(page, perPage);
+  }
+
+  @Get('filter')
+  findWithFilter(
+    @Query("page") page: number,
+    @Query("perPage") perPage: number,
+    @Query("name") name?: string,
+    @Query("mileage") mileage?: string,
+    @Query("year") year?: number,
+    @Query("maxPrice") maxPrice?: number,
+    @Query("engine") engine?: string,
+    @Query("driveType") driveType?: string,
+    @Query("numberOfPeople") numberOfPeople?: number,
+    @Query("numberOfDoors") numberOfDoors?: number
+  ) {
+    year = year ? Number(year) : undefined;
+    numberOfPeople = numberOfPeople ? Number(numberOfPeople) : undefined;
+    numberOfDoors = numberOfDoors ? Number(numberOfDoors) : undefined;
+    maxPrice = maxPrice ? Number(maxPrice) : undefined;
+
+    return this.carsService.findWithFilter(
+      page,
+      perPage,
+      name,
+      mileage,
+      year,
+      maxPrice,
+      engine,
+      driveType,
+      numberOfPeople,
+      numberOfDoors
+    );
+  }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.carsService.findOne(id);
   }
 }
--- src/backend/src/cars/cars.service.ts
+++ src/backend/src/cars/cars.service.ts
@@ -5,9 +5,84 @@ import { PrismaService } from "src/prisma/prisma.service";
 export class CarsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findAll(page: number, perPage: number = 12) {
+  async findWithFilter(
+    page: number = 1,
+    perPage = 10,
+    name?: string,
+    mileage?: string,
+    year?: number,
+    maxPrice?: number,
+    engine?: string,
+    driveType?: string,
+    numberOfPeople?: number,
+    numberOfDoors?: number
+  ) {
+    const filters: any = {};
+
+    if (name) {
+      filters.name = {
+        contains: name,
+        mode: "insensitive",
+      };
+    }
+
+    if (mileage) {
+      filters.mileage = {
+        contains: mileage,
+        mode: "insensitive",
+      };
+    }
+
+    if (year) {
+      filters.year = {
+        equals: year,
+      };
+    }
+
+    if (maxPrice) {
+      filters.price = {
+        lte: maxPrice,
+      };
+    }
+
+    if (engine) {
+      filters.engine = {
+        contains: engine,
+        mode: "insensitive",
+      };
+    }
+
+    if (driveType) {
+      filters.driveType = {
+        contains: driveType,
+        mode: "insensitive",
+      };
+    }
+
+    if (numberOfPeople) {
+      filters.numberOfPeople = {
+        equals: numberOfPeople,
+      };
+    }
+
+    if (numberOfDoors) {
+      filters.numberOfDoors = {
+        equals: numberOfDoors,
+      };
+    }
+
     const cars = await this.prismaService.car.findMany({
-      skip: page * perPage,
+      skip: (page - 1) * perPage,
+      take: perPage,
+      where: filters,
+    });
+
+    return cars;
+  }
+
+  async findAll(page: number = 1, perPage = 10) {
+    const cars = await this.prismaService.car.findMany({
+      skip: (page - 1) * perPage,
       take: perPage,
     });
 
