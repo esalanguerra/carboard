1230e9c215941a87c2be44c31485d37541a58a91
Alan Guerra <esalanguerra@gmail.com>
2024-12-19 07:54:56 -0300

    compressão de dados

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   app.enableCors();
+
+  app.use(
+    compression({
+      level: 6,
+      threshold: 1024,
+      filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
+          return false;
+        }
+
+        return compression.filter(req, res);
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
